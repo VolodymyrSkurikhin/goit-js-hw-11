7b1b58bb2d32a417a399105de363630afe784659
Volodymyr <vladiskuri@gmail.com>
2022-04-16 14:16:28 +0300

    next initial

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+let KEY ='26793490-dae10d4013ec617276bbdd3a4';
+let search;
+axios.defaults.baseURL = 'https://pixabay.com/api/?key=${KEY}&image_type="photo"&orientation="horizontal"&safesearch="true"';
+
+
